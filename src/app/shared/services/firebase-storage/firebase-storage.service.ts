6c8aba8ab959ad85ec5b19047f38784ea10162b2
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-07-02 12:38:26 +0800

    feat: 1. Add city-country pair destinations. 2. Build index on Firestore. 3. Adjust HTML templates and use placeholder.png. 4. Batch upload destinations to Firestore.

--- src/app/shared/services/firebase-storage/firebase-storage.service.ts
+++ src/app/shared/services/firebase-storage/firebase-storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { bucket } from '../../../firebase/bucket';
 import { getDownloadURL, ref } from 'firebase/storage';
-import { defer, from, of } from 'rxjs';
+import { catchError, defer, from, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +10,13 @@ export class FirebaseStorageService {
   constructor() {}
 
   getImage(name: string) {
-    return defer(() => from(getDownloadURL(ref(bucket, `images/${name}`))));
+    return defer(() =>
+      from(getDownloadURL(ref(bucket, `images/${name}`))).pipe(
+        catchError((error) => {
+          console.log('Error getting image', error);
+          return of('');
+        })
+      )
+    );
   }
 }
