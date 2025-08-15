063f7b9945d1cfbbf40a5b0efb812ce543f69779
MSI\guanxin <guanxinwang0118@gmail.com>
2025-08-15 15:53:07 +0800

    fix: ng version upgrade

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
     selector: 'app-footer',
-    imports: [CommonModule],
+    imports: [],
     template: `
     <footer class="footer">
       <div class="container">
